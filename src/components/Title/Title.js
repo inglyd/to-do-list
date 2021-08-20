7bcde5f3c126e59e750023dae002be5886fadc03
inglyd <inglydalves@outlook.com>
2021-08-19 23:23:57 -0300

    criando o componente Title

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Title.css'
+const Title = () => {
+   return (
+<>
+<div className='title'>
+  <h1>Minhas Tarefas</h1>
+</div>
+</>
+  )
+}
+
+export default Title;
